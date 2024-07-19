b83814a0fb189c74c8b9831f11f218709f9afd9a
Shachar <60000798+OcammsRazor@users.noreply.github.com>
2024-07-19 11:47:24 +0300

    split table row to component

--- src/components/excercise-1/FirstExcercise.tsx
+++ src/components/excercise-1/FirstExcercise.tsx
@@ -1,18 +1,37 @@
-import {useFindLeastPopularCharacter, useGetLocationByName} from './first-excercise.hooks';
-import texts from './first-excercise.texts.json';
+import {
+  useFindLeastPopularCharacter,
+  useGetLocationByName,
+} from "./first-excercise.hooks";
+import { TableRow } from "./table-row/TableRow";
+import texts from "./first-excercise.texts.json";
+import styles from "./first-excercise.module.scss";
 
-const LOCATION_NAME = 'Earth (C-137)';
+const LOCATION_NAME = "Earth (C-137)";
 
 export const FirstExcercise = () => {
-    const locationData = useGetLocationByName(LOCATION_NAME);
-    const leastPopular = useFindLeastPopularCharacter(locationData?.residentsIds);
+  const locationData = useGetLocationByName(LOCATION_NAME);
+  const leastPopular = useFindLeastPopularCharacter(locationData?.residentsIds);
 
-    return (
-        <div>
-            <h3>{texts.title}</h3>
-            <div>
-                table should be here
-            </div>
+  const tableData = {
+    name: leastPopular?.name,
+    location: `${locationData?.name} & ${locationData?.dimension}`,
+    status: leastPopular?.status,
+    species: leastPopular?.species,
+    gender: leastPopular?.gender,
+    popularity: leastPopular?.episode.length,
+  };
+
+  return (
+    <div className={styles.firstExcercise}>
+      <h3>{texts.title}</h3>
+      <div className={styles.table}>
+        <img src={leastPopular?.image} />
+        <div className={styles.tableContent}>
+          {texts.tableFields.map((field, index) => (
+            <TableRow category={field.title} value={tableData[field.apiKey as keyof typeof tableData]} key={`${field.apiKey}-${index}`}/>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
