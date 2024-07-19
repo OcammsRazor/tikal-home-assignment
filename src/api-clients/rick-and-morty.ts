b010476a956fb75ea40c409e5e9b3505ffd3b9ab
Shachar <60000798+OcammsRazor@users.noreply.github.com>
2024-07-19 14:22:09 +0300

    add basic structure for second challenge and connect it to menu

--- src/api-clients/rick-and-morty.ts
+++ src/api-clients/rick-and-morty.ts
@@ -1,15 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const DEFAULT_URL = 'https://rickandmortyapi.com/api';
+export const DEFAULT_URL = "https://rickandmortyapi.com/api";
 
-const characterNamesToQueryParam = (characherNames: string[]) => characherNames.join('&name=');
+const characterNamesToQueryParam = (characherNames: string[]) =>
+  characherNames.join("&name=");
 
 export const rickAndMortyClient = (baseUrl = DEFAULT_URL) => {
-    const client = axios.create({baseURL: baseUrl});
+  const client = axios.create({ baseURL: baseUrl });
 
-    return ({
-        getLocationByName: (locationName: string)=> client.get(`/location?name=${locationName}`),
-        getCharactersData: (characterIds: string[]) => client.get(`/character/${characterIds.toString()}`),
-        getMultipleCharactersByNames: (characterNames: string[]) => client.get(`/character?name=${characterNamesToQueryParam(characterNames)}`),
-    })
-}
+  return {
+    getLocationByName: (locationName: string) =>
+      client.get(`/location?name=${locationName}`),
+    getCharactersData: (characterIds: string[]) =>
+      client.get(`/character/${characterIds.toString()}`),
+    getMultipleCharactersByNames: (characterNames: string[]) =>
+      client.get(
+        `/character?name=${characterNamesToQueryParam(characterNames)}`
+      ),
+  };
+};
---
+++ src/components/excercise-2/SecondExcercise.tsx
@@ -0,0 +1,11 @@
+import texts from "./second-excercise.texts.json";
+
+export const SecondExcercise = () => {
+  return (
+    <div>
+      <h3>{texts.title}</h3>
+      <h4>{texts.subtitle}</h4>
+      <div></div>
+    </div>
+  );
+};
