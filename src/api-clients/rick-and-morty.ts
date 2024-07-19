75100b9ab504411d2a4106ffd6681dffc78c3893
Shachar <60000798+OcammsRazor@users.noreply.github.com>
2024-07-19 14:17:25 +0300

    add new route to fetch characters by names

--- src/api-clients/rick-and-morty.ts
+++ src/api-clients/rick-and-morty.ts
@@ -2,11 +2,14 @@ import axios from 'axios';
 
 export const DEFAULT_URL = 'https://rickandmortyapi.com/api';
 
+const characterNamesToQueryParam = (characherNames: string[]) => characherNames.join('&name=');
+
 export const rickAndMortyClient = (baseUrl = DEFAULT_URL) => {
     const client = axios.create({baseURL: baseUrl});
 
     return ({
         getLocationByName: (locationName: string)=> client.get(`/location?name=${locationName}`),
-        getCharactersData: (characterIds: string[]) => client.get(`/character/${characterIds.toString()}`)
+        getCharactersData: (characterIds: string[]) => client.get(`/character/${characterIds.toString()}`),
+        getMultipleCharactersByNames: (characterNames: string[]) => client.get(`/character?name=${characterNamesToQueryParam(characterNames)}`),
     })
 }
